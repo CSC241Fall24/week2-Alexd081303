c598ff5d855788c61c1f0f9136bc7cc5fa87a4f2
Alexd081303 <143731771+Alexd081303@users.noreply.github.com>
2024-09-06 11:54:16 -0400

    Update Q4ReverseString.java

--- src/main/java/Q4ReverseString.java
+++ src/main/java/Q4ReverseString.java
@@ -2,10 +2,18 @@ public class Q4ReverseString
 {
     public static String reverse(String s) 
     {
-        if (s.length() == 1)
+        if (s.length() <= 1)
         {
             return s;
         }
+        
         return reverse(s.substring(1)) + s.charAt(0);
+    }
+
+    public static void main(String[] args) 
+    {
+        String test = "test string";
+        System.out.println("Original: " + test);
+        System.out.println("Reversed: " + reverse(test));
     }
 }
