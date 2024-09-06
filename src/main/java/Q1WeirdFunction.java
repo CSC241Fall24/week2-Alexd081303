00650dd1fee3eb67dbfbd6ff59e9a8d9a0bc5990
Alexd081303 <143731771+Alexd081303@users.noreply.github.com>
2024-09-06 12:04:12 -0400

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -25,7 +25,7 @@ public class Q1WeirdFunction {
     }
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 5; 
         System.out.println("Recursive result for n = " + n + ": " + fRecursive(n));
         System.out.println("Iterative result for n = " + n + ": " + fIterative(n));
     }
