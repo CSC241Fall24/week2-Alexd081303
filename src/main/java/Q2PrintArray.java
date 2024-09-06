0944276ed6585b34f30d5b4a6f9937135eb70305
Alexd081303 <143731771+Alexd081303@users.noreply.github.com>
2024-09-06 11:46:14 -0400

    Update Q2PrintArray.java

--- src/main/java/Q2PrintArray.java
+++ src/main/java/Q2PrintArray.java
@@ -1,15 +1,17 @@
-public class Q2PrintArray {
-    private static int[] array = {1, 2, 3, 4, 5, 6, 7};
+public class Q2PrintArray 
+{
+    private static final int[] ARRAY = {1, 2, 3, 4, 5, 6, 7};
 
     public static void main(String[] args) {
-        printRecursion(array.length);
+        printRecursion(0);
     }
 
-    private static void printRecursion(int i) {
-            if (i <= 0) {
+    private static void printRecursion(int i) 
+    {
+        if (i >= ARRAY.length) {
             return;
         }
-        System.out.println(array[i - 1]);
-        printRecursion(i - 1);
+        System.out.println(ARRAY[i]);
+        printRecursion(i + 1);
     }
 }
